8b0fe91d6ac975f90fd9ba2b2b4beeaa0b3b03ff
Peterson Santos <sdpeterson19@gmail.com>
2022-12-10 11:52:00 -0300

    Aula11 cursoJS-Sistemas de multa e Sistema de nacionalidade

---
+++ exe008.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`A velcidade do seu carro é ${vel}Km/h`)
+if (vel > 60) {
+    console.log('Voçê ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Dirija sempre usando cinto de segurança')
---
+++ exe009.js
@@ -0,0 +1,7 @@
+var país = 'EUA'
+console.log(`Vivendo em ${país}`)
+if(país == 'Brasil') {
+    console.log('Voçê é brasileiro!')
+} else {
+    console.log('Voçê é estrangeiro!')
+}
